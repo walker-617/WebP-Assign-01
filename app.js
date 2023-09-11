a970f2edc65106023dd6cc717ce831b0ec7cf610
WALKER <divakarnaidumahanthi@gmail.com>
2023-09-11 19:55:54 +0530

    added student.html and home.html

--- app.js
+++ app.js
@@ -6,10 +6,22 @@ const app=express();
 app.use(express.static(__dirname));
 
 app.get('/',(req,res)=>{
+
     res.sendFile(__dirname+"/static/root.html");
 })
 
-app.get('/group',(req,res)=>{
+app.get('/student',(req,res)=>{
+    if (fs.existsSync(__dirname+"/static/student.html"))
+    {
+        res.sendFile(__dirname+"/static/student.html");
+    }
+    else
+    {
+        res.redirect("/error")
+    }
+})
+
+app.get('/student.html',(req,res)=>{
     if (fs.existsSync(__dirname+"/static/student.html"))
     {
         res.sendFile(__dirname+"/static/student.html");
@@ -21,6 +33,11 @@ app.get('/group',(req,res)=>{
 })
 
 app.get('/home',(req,res)=>{
+    res.status(301);
+    res.redirect("https://www.google.com");
+})
+
+app.get('/home.html',(req,res)=>{
     res.status(301);
     res.redirect("https://www.google.com");
 })
